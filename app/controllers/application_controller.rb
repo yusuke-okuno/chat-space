1359c07c83586ad4dfdf73057cce2bef9420cac8
yusuke-okuno <yusuke0920okuno@gmail.com>
2019-05-27 19:45:40 +0900

    make chat-space view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  config.generators do |g|
+    g.stylesheets false
+    g.javascripts false
+    g.helper false
+    g.test_framework false
+  end
 end
