a0988d8679669d01cc3403dfd2cebaf7c9b415c7
yusuke-okuno <yusuke0920okuno@gmail.com>
2019-06-04 16:49:51 +0900

    make group

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'messages#index'
-
+  devise_for :users
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
